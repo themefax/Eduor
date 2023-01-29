8f9bbae7b348d4c8ccd2a96e8516e53a09940633
freelancersazal <freelancersazal@gmail.com>
2023-01-30 01:09:56 +0600

    added four new pages

--- js/main.js
+++ js/main.js
@@ -29,8 +29,6 @@ $(function () {
         autoplaySpeed: 4000,
         dots: true,
         arrows: false,
-        // nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
-        // prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
 
         responsive: [
             {
@@ -205,7 +203,7 @@ $(function () {
     $('.brand_slider').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4000,
         dots: false,
         arrows: false,
@@ -262,6 +260,49 @@ $(function () {
         }
     });
 
+
+    //=======POPULAR SERVICE SLIDER======
+    $('.popular_service_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
 
